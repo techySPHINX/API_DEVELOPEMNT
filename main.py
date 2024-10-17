757d4442c00df0587d35e8340324feeea1279cfe
techySphinx <shimon123universe@gmail.com>
2024-10-17 13:15:56 +0530

    feat: get through path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -166,4 +166,14 @@ async def hidden_query_route(
     return {"hidden_query": "Not found"}
 
 
-
+@app.get("/items_validation/{item_id}")
+async def read_items_validation(
+    *,
+    item_id: int = Path(..., title="The ID of the item to get", gt=10, le=100),
+    q: str = "hello",
+    size: float = Query(..., gt=0, lt=7.75)
+):
+    results = {"item_id": item_id, "size": size}
+    if q:
+        results.update({"q": q})
+    return results
