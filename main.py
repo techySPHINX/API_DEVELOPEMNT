03d744f9b290222bd1a86ffa895353c3536a8e5b
techySphinx <shimon123universe@gmail.com>
2024-10-17 13:08:34 +0530

    feat: get through params

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -137,3 +137,33 @@ async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
     if q:
         result.update({"q": q})
     return result
+
+
+@app.get("/items")
+async def read_items(
+    q: str
+    | None = Query(
+        None,
+        min_length=3,
+        max_length=10,
+        title="Sample query string",
+        description="This is a sample query string.",
+        alias="item-query",
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items_hidden")
+async def hidden_query_route(
+    hidden_query: str | None = Query(None, include_in_schema=False)
+):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    return {"hidden_query": "Not found"}
+
+
+
